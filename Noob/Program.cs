cf3b1e33e95d084ec2afa56522f9d52ba2d12922
DyudinAlexey <dyudin-aleksei@mail.ru>
2017-10-03 23:05:49 +0300

    qwert

---
+++ Noob/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenhgt = 10;
+            int[] arr = new int[lenhgt];
+
+            for (int i = 0; i < 10; i++) {
+                arr[i] = i;
+                Console.Write(arr[i]);
+                
+            }
+
+
+                Console.ReadKey();
+        }
+    }
+}
